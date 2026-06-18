0dcdb4e9cafc4fcb32bf55eb5556c0bde0c490c0
mkekn23 <mkekn23@freeuni.edu.ge>
2026-06-18 17:04:03 +0400

    feat: complete backend auth and frontend registration and login, also added user table to database"

--- backend/src/main/java/com/serialtracker/backend/dto/LoginResponse.java
+++ backend/src/main/java/com/serialtracker/backend/dto/LoginResponse.java
@@ -6,7 +6,6 @@ public class LoginResponse {
     public LoginResponse(String token) {
         this.token = token;
     }
-
     public String getToken() { return token; }
     public void setToken(String token) { this.token = token; }
 }
