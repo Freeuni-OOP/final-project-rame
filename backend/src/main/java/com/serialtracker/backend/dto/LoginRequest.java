a88fcfe4fa0402a81ee7fd4953b7ed9a61a458cd
mkekn23 <mkekn23@freeuni.edu.ge>
2026-06-21 23:33:19 +0400

    feat: added series tracking back and ugly frontend

--- backend/src/main/java/com/serialtracker/backend/dto/LoginRequest.java
+++ backend/src/main/java/com/serialtracker/backend/dto/LoginRequest.java
@@ -29,7 +29,7 @@ public class LoginRequest {
     public void setUsername(String username) { this.username = username; }
 
     public String getPassword() { return password; }
-    public voidsetPassword(String password) { this.password = password; }
+    public void setPassword(String password) { this.password = password; }
 
     public String getEmail() {return email;}
     public void setEmail(String email){this.email = email;}
